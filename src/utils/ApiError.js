479621f676a7cbe8cc9939d91b1e362ef917e71d
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-10 18:00:33 +0530

    utilitie code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statuscode,
+    message = "SomeThing went Wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    (this.statuscode = statuscode),
+      (this.data = null),
+      (this.message = message),
+      (this.success = false),
+      (this.errors = errors);
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { ApiError };
